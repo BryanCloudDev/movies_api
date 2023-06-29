4ed2851890f04d7f29bb7a2a39ffbedb24306b2d
Bryan Portillo <bportillo701@gmail.com>
2023-06-29 00:37:59 -0600

    adding role model

---
+++ models/Role.ts
@@ -0,0 +1,12 @@
+import { Entity, Column, OneToMany } from 'typeorm'
+import { BaseModel } from './BaseModel'
+import { User } from './User'
+
+@Entity()
+export class Role extends BaseModel {
+  @Column()
+    name: string
+
+  @OneToMany(() => User, (user) => user.role)
+    user: User[]
+}
