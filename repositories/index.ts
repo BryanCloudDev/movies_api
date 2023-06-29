03847c11341782832d04a3f667f371bfe8a28197
Bryan Portillo <bportillo701@gmail.com>
2023-06-29 02:24:02 -0600

    adding role repository

--- repositories/index.ts
+++ repositories/index.ts
@@ -1,12 +1,14 @@
 import { AppDataSource } from '../database'
-import { LikedMovie, Movie, User } from '../models'
+import { LikedMovie, Movie, Role, User } from '../models'
 
 const userRepository = AppDataSource.getRepository(User)
 const movieRepository = AppDataSource.getRepository(Movie)
 const likedMoviesRepository = AppDataSource.getRepository(LikedMovie)
+const roleRepository = AppDataSource.getRepository(Role)
 
 export {
   userRepository,
   movieRepository,
-  likedMoviesRepository
+  likedMoviesRepository,
+  roleRepository
 }
