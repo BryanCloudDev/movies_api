1e8fb51f12268216e9f8480c9050ae5168279c4c
Bryan Portillo <bportillo701@gmail.com>
2023-06-29 03:37:33 -0600

    adding getRoleById service

--- services/role/index.ts
+++ services/role/index.ts
@@ -1,12 +1,19 @@
+import { type Role } from '../../models'
 import { roleRepository } from '../../repositories'
 
 const isRolevalid = async (id: number): Promise<boolean | undefined> => {
   if (id === null) return false
 
-  const role = await roleRepository.findOne({ where: { id } })
+  const role = await getRoleByIdService(id)
   if (role === null) throw new Error('The role is not registered in DB')
 }
 
+const getRoleByIdService = async (id: number): Promise<Role | null> => {
+  const role = await roleRepository.findOne({ where: { id } })
+  return role
+}
+
 export {
-  isRolevalid
+  isRolevalid,
+  getRoleByIdService
 }
