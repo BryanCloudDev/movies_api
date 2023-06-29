6ddefa84a307a2d4e9891fddc375597ff336001d
Bryan Portillo <bportillo701@gmail.com>
2023-06-29 03:37:05 -0600

    adding fix in password encryption service

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -1,13 +1,14 @@
 
 import bcrypt from 'bcryptjs'
 
-const encrypt = async (text: string): Promise<string | undefined> => {
+const encrypt = async (text: string): Promise<string> => {
   try {
     const salt = await bcrypt.genSalt()
     const password = await bcrypt.hash(text, salt)
     return password
   } catch (error: any) {
     console.log(error)
+    return ''
   }
 }
 
