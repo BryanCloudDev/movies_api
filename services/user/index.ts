ca61b92b5adea1d4ff89cf9b871235ebe375b526
Bryan Portillo <bportillo701@gmail.com>
2023-06-29 03:37:53 -0600

    adding create user and create instance of user

--- services/user/index.ts
+++ services/user/index.ts
@@ -1,5 +1,8 @@
-import { type User } from '../../models'
+import { type IUserRequest } from '../../dto/user/IUserRequest'
+import { User } from '../../models'
 import { userRepository } from '../../repositories'
+import { encrypt } from '../auth'
+import { getRoleByIdService } from '../role'
 
 const existsUserById = (id: number): void => {
   const user = getUserbyIdService(id)
@@ -11,6 +14,22 @@ const getUserbyIdService = async (id: number): Promise<User | null> => {
   return user
 }
 
+const createUserInstanceService = async (userRequest: IUserRequest, roleId: number): Promise<User> => {
+  const role = await getRoleByIdService(roleId)
+
+  if (role !== null) {
+    userRequest.password = await encrypt(userRequest.password)
+    const user = userRepository.create({ ...userRequest, role })
+    return user
+  }
+  return new User()
+}
+
+const createUserService = async (user: User): Promise<User> => {
+  const createdUser = await userRepository.save(user)
+  return createdUser
+}
+
 const emailExists = async (email: string): Promise<void> => {
   const user = await userRepository.findOne({ where: { email } })
   if (user !== null) throw new Error(`The email ${email} is already registered in DB`)
@@ -19,5 +38,7 @@ const emailExists = async (email: string): Promise<void> => {
 export {
   existsUserById,
   getUserbyIdService,
+  createUserInstanceService,
+  createUserService,
   emailExists
 }
