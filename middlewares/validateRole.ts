a5c3937fac48e23636788954d46d2b4a2d1a5876
Bryan Portillo <bportillo701@gmail.com>
2023-06-30 16:10:18 -0600

    adding access by role to user endpoints

---
+++ middlewares/validateRole.ts
@@ -0,0 +1,20 @@
+import { type NextFunction, type Request, type Response } from 'express'
+import type Roles from '../dto/enums/roles'
+import { type User } from '../models'
+
+const validateRole = (roles: Roles[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { role }: User = req.user as User
+    const { id } = role
+
+    if (!roles.includes(id)) {
+      return res.status(403).json({
+        message: 'You are not authorized'
+      })
+    }
+
+    next()
+  }
+}
+
+export default validateRole
--- routes/user.ts
+++ routes/user.ts
@@ -6,16 +6,21 @@ import { createUser, getUserbyId } from '../controllers'
 import { isRolevalid } from '../services/role'
 import { deleteUser, getAllUsers, getUserProfile, updateUser } from '../controllers/user'
 import { validateJWT } from '../middlewares/validateJWT'
+import validateRole from '../middlewares/validateRole'
+import Roles from '../dto/enums/roles'
 
 const userRouter = Router()
 
 userRouter.get('/profile',
   [
-    validateJWT
+    validateJWT,
+    validateRole([Roles.ADMIN, Roles.USER])
   ], getUserProfile)
 
 userRouter.get('/:id',
   [
+    validateJWT,
+    validateRole([Roles.ADMIN]),
     param('id').isNumeric(),
     param('id').custom(existsUserById),
     validateFields
@@ -33,22 +38,31 @@ userRouter.post('/',
     validateFields
   ], createUser)
 
-userRouter.patch('/:id', [
-  param('id').isNumeric(),
-  param('id').custom(existsUserById),
-  validateEmailInChange,
-  body('roleId').custom(isRolevalid),
-  validateFields
-], updateUser)
+userRouter.patch('/:id',
+  [
+    validateJWT,
+    validateRole([Roles.ADMIN]),
+    param('id').isNumeric(),
+    param('id').custom(existsUserById),
+    validateEmailInChange,
+    body('roleId').custom(isRolevalid),
+    validateFields
+  ], updateUser)
 
 userRouter.delete('/:id',
   [
     validateJWT,
+    validateRole([Roles.ADMIN]),
     param('id').isNumeric(),
     param('id').custom(existsUserById),
     validateFields
   ], deleteUser)
 
-userRouter.get('/', getAllUsers)
+userRouter.get('/',
+  [
+    validateJWT,
+    validateRole([Roles.ADMIN]),
+    validateFields
+  ], getAllUsers)
 
 export default userRouter
