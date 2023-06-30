1f9070228a093108f8d671142f6b31e7eabce56c
Bryan Portillo <bportillo701@gmail.com>
2023-06-30 17:54:43 -0600

    adding validation login if user banned/inactivated

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -2,12 +2,27 @@ import { type NextFunction, type Request, type Response } from 'express'
 import { passport } from '../services/passport/localStrategy'
 import { type User } from '../models'
 import { generateJWT } from '../services/auth'
+import { Status } from '../dto/enums/status'
 
 const login = (req: Request, res: Response, next: NextFunction): void => {
   passport.authenticate('local', async (err: Error | null, user: User | false, info: any) => {
     try {
       if (err !== null || user === false) {
         return res.json(info)
+      }
+
+      const status = user.status
+
+      if (status === Status.INACTIVE) {
+        return res.json({
+          message: 'Your account has been deleted'
+        })
+      }
+
+      if (status === Status.BANNED) {
+        return res.json({
+          message: 'Your account has been banned'
+        })
       }
 
       req.login(user, { session: false }, async (err: any) => {
