819c3b7fefb574d16f40c44f6ffa17c1f4e484e2
Bryan Portillo <bportillo701@gmail.com>
2023-06-30 14:57:17 -0600

    adding movie router to index

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,9 @@
-import userRouter from './user'
 import authRouter from './auth'
+import movieRouter from './movie'
+import userRouter from './user'
 
 export {
   authRouter,
+  movieRouter,
   userRouter
 }
