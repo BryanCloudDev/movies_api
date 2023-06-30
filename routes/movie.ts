7fcafd0f98bc925c11fc3fe275a5db8010472ccf
Bryan Portillo <bportillo701@gmail.com>
2023-06-30 16:12:18 -0600

    adding access by role to movie endpoints

--- routes/movie.ts
+++ routes/movie.ts
@@ -2,18 +2,24 @@ import { Router } from 'express'
 import { createMovie, deleteMovie, updateMovie } from '../controllers/movie'
 import { param } from 'express-validator'
 import { validateFields } from '../middlewares/validateFields'
+import { validateJWT } from '../middlewares/validateJWT'
+import validateRole from '../middlewares/validateRole'
+import Roles from '../dto/enums/roles'
 
 const movieRouter = Router()
 
 movieRouter.post('/',
   [
-
+    validateJWT,
+    validateRole([Roles.ADMIN])
   ],
   createMovie
 )
 
 movieRouter.patch('/:id',
   [
+    validateJWT,
+    validateRole([Roles.ADMIN]),
     param('id').isNumeric(),
     validateFields
   ],
@@ -22,6 +28,8 @@ movieRouter.patch('/:id',
 
 movieRouter.delete('/:id',
   [
+    validateJWT,
+    validateRole([Roles.ADMIN]),
     param('id').isNumeric(),
     validateFields
   ],
