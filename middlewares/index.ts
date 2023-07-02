886c50d73d52d33bb203e04c4705bd1c392407e8
Bryan Portillo <bportillo701@gmail.com>
2023-07-02 00:55:53 -0600

    adding export default for JWT validation

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,7 +1,7 @@
 import validateEmailInChange from './validateEmailInChange'
 import validateFields from './validateFields'
 import validateIdMovie from './validateIdMovie'
-import { validateJWT } from './validateJWT'
+import validateJWT from './validateJWT'
 import validateQuery from './validateQuery'
 import validateRole from './validateRole'
 
