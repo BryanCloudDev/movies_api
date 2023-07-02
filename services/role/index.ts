55375f30fc4ded638a98827a5d1eaac7229a5879
Bryan Portillo <bportillo701@gmail.com>
2023-07-02 02:23:59 -0600

    removing service to check role

--- services/role/index.ts
+++ services/role/index.ts
@@ -2,23 +2,15 @@ import { type Role } from '../../models'
 import { roleRepository } from '../../repositories'
 import errorMessageHandler from '../errorMessage'
 
-const isRolevalid = async (id: number): Promise<boolean | undefined> => {
-  if (id === null) return false
-
-  const role = await getRoleByIdService(id)
-  if (role === null) throw new Error('The role is not registered in DB')
-}
-
-const getRoleByIdService = async (id: number): Promise<Role | null | undefined> => {
+const getRoleByIdService = async (id: number): Promise<Role | null | undefined | { message: string }> => {
   try {
     const role = await roleRepository.findOne({ where: { id } })
     return role
   } catch (error: any) {
-    errorMessageHandler(error, 'Error in get role by id service')
+    return errorMessageHandler(error, 'Error in get role by id service')
   }
 }
 
 export {
-  isRolevalid,
   getRoleByIdService
 }
