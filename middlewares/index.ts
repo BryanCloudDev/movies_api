97b2c49bf43f427c48f787acab13d4acfb791c9e
Bryan Portillo <bportillo701@gmail.com>
2023-07-02 00:52:21 -0600

    adding index for middleware

---
+++ middlewares/index.ts
@@ -0,0 +1,15 @@
+import validateEmailInChange from './validateEmailInChange'
+import validateFields from './validateFields'
+import validateIdMovie from './validateIdMovie'
+import { validateJWT } from './validateJWT'
+import validateQuery from './validateQuery'
+import validateRole from './validateRole'
+
+export {
+  validateEmailInChange,
+  validateFields,
+  validateIdMovie,
+  validateJWT,
+  validateQuery,
+  validateRole
+}
