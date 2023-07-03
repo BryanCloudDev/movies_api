3b885e7c1ded383db5b9ffc42ef491660a6eb89f
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 06:39:08 -0600

    adding login validations

--- controllers/user.ts
+++ controllers/user.ts
@@ -44,7 +44,6 @@ const getAllUsers = async (req: ICustomRequest, res: Response): Promise<Response
     }
 
     const users = await userRepository.find()
-
     return res.status(200).json(users)
   } catch (error: any) {
     return res.status(500).json(errorMessageHandler(error, 'Error in get all users'))
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express'
 import { login } from '../controllers'
+import { body } from 'express-validator'
+import { validateFields } from '../middlewares'
 
 const authRouter = Router()
 
-authRouter.post('/', login)
+authRouter.post('/',
+  [
+    body('email', 'The email address is not valid').isEmail(),
+    body('password', 'Password is required').notEmpty(),
+    validateFields
+  ],
+  login
+)
 
 export default authRouter
