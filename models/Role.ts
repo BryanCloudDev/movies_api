9b95115aec56b11d8e156c1a9c543d24db4a1174
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 01:28:05 -0600

    adding status prop to model

--- models/Role.ts
+++ models/Role.ts
@@ -1,11 +1,15 @@
 import { Column, Entity, OneToMany } from 'typeorm'
 import BaseModel from './BaseModel'
 import User from './User'
+import { Status } from '../dto'
 
 @Entity()
 export default class Role extends BaseModel {
   @Column()
     name: string
+
+  @Column({ default: Status.ACTIVE })
+    status: number
 
   @OneToMany(() => User, (user) => user.role)
     user: User[]
