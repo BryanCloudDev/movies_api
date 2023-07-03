4e8179b02cc1277742abe0a000fbc9f61fbfaf00
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 01:44:48 -0600

    adding role router to index

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,13 @@
 import authRouter from './auth'
 import explorerRouter from './explorer'
 import movieRouter from './movie'
+import roleRouter from './role'
 import userRouter from './user'
 
 export {
   authRouter,
   explorerRouter,
   movieRouter,
+  roleRouter,
   userRouter
 }
