12c7371665bce085de9f51cf0afe3a7c9b48d5b3
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 07:05:54 -0600

    adding validation when status in patch is not sent

--- middlewares/validateStatus.ts
+++ middlewares/validateStatus.ts
@@ -2,6 +2,11 @@ import { type NextFunction, type Request, type Response } from 'express'
 import { Status } from '../dto'
 
 const validateStatus = (req: Request, res: Response, next: NextFunction): Response | undefined => {
+  if (req.body.status === undefined) {
+    next()
+    return
+  }
+
   const statusId = parseInt(req.body.status)
 
   if (!(statusId in Status)) {
