68e0fa1c93ce8579daded853a66a62308d3aac74
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 01:28:59 -0600

    adding delete role and get all roles controllers

--- controllers/role.ts
+++ controllers/role.ts
@@ -1,6 +1,8 @@
-import { type IRoleRequest } from '../dto'
+import { type IRoleRequest, Status } from '../dto'
 import { type Request, type Response } from 'express'
-import { createRoleInstanceService, createRoleService, errorMessageHandler } from '../services'
+import { createFilter, createRoleInstanceService, createRoleService, errorMessageHandler } from '../services'
+import { roleRepository } from '../repositories'
+import { Role } from '../models'
 
 const createRole = async (req: Request, res: Response): Promise<Response> => {
   try {
@@ -17,16 +19,38 @@ const createRole = async (req: Request, res: Response): Promise<Response> => {
   }
 }
 
-const deleteRole = (req: Request, res: Response) => {
+const deleteRole = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const id = parseInt(req.params.id)
 
+    await roleRepository.update(id, {
+      status: Status.INACTIVE
+    })
+
+    return res.status(204).json({})
+  } catch (error: any) {
+    return res.status(500).json(errorMessageHandler(error, 'Error in delete movie'))
+  }
 }
 
-const getRoles = (req: Request, res: Response) => {
+const getAllRoles = async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const reqFilter = req.query.filter as string
+    if (reqFilter !== undefined) {
+      const roles = await createFilter(reqFilter, new Role(), roleRepository)
+      return res.status(200).json(roles)
+    }
 
+    const roles = await roleRepository.find()
+
+    return res.status(200).json(roles)
+  } catch (error: any) {
+    return res.status(500).json(errorMessageHandler(error, 'Error in get all roles'))
+  }
 }
 
 export {
   createRole,
   deleteRole,
-  getRoles
+  getAllRoles
 }
