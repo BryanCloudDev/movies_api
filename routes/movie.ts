6bb4e023caea83cf3e6bf94f33ea58a9eccc617c
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 07:06:17 -0600

    adding movie route validations

--- routes/movie.ts
+++ routes/movie.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { body, param } from 'express-validator'
 import { createMovie, deleteMovie, getAllMovies, getLikeCountForMovies, likeAMovie, unlikeAMovie, updateMovie } from '../controllers'
 import { validateFields, validateIdMovie, validateJWT, validateLikedMovieonCreate, validateLikedMovieonDelete, validateRole, validateStatus } from '../middlewares'
+import { moviePatchValidations, moviePostValidations } from '../services/movie'
 
 const movieRouter = Router()
 
@@ -50,7 +51,7 @@ movieRouter.patch('/:id',
   [
     validateJWT,
     validateRole([Roles.ADMIN]),
-    param('id').isNumeric(),
+    ...moviePatchValidations,
     validateFields,
     validateIdMovie,
     validateStatus
@@ -62,16 +63,7 @@ movieRouter.post('/',
   [
     validateJWT,
     validateRole([Roles.ADMIN]),
-    body('countryOrigin', 'The country of origin is mandatory').notEmpty().trim(),
-    body('description', 'The description is mandatory').notEmpty().trim(),
-    body('director', 'The director is mandatory').notEmpty().trim(),
-    body('duration', 'The duration must be an integer').isInt(),
-    body('genre', 'The genre is mandatory').notEmpty().trim(),
-    body('language', 'The language is mandatory').notEmpty().trim(),
-    body('name', 'The name is mandatory').notEmpty().trim(),
-    body('poster', 'The poster must be a valid URL').isURL().trim(),
-    body('rating', 'The rating must be a float').isFloat(),
-    body('releaseDate', 'The release date must be a valid date').isISO8601(),
+    ...moviePostValidations,
     validateFields
   ],
   createMovie
--- services/movie/index.ts
+++ services/movie/index.ts
@@ -2,6 +2,7 @@ import { type IMovieLikeCount, type IMovieRequest } from '../../dto'
 import { type Movie } from '../../models'
 import { errorMessageHandler } from '../'
 import { movieRepository } from '../../repositories'
+import { body, param } from 'express-validator'
 
 const createMovieInstanceService = (movieRequest: IMovieRequest): Movie => {
   try {
@@ -35,8 +36,38 @@ const getLikeCountService = (movies: Movie[]): IMovieLikeCount[] => {
   return likedMovies
 }
 
+const movieOptionalValidations = [
+  body('countryOrigin', 'The country must have a valid value').optional().notEmpty().trim(),
+  body('director', 'The director must have a valid value').optional().notEmpty().trim(),
+  body('duration', 'The duration must have a valid value').optional().isInt(),
+  body('poster', 'The poster must be a valid URL').optional().isURL().trim(),
+  body('releaseDate', 'The release date must be a valid date').optional().isISO8601()
+]
+
+const moviePatchValidations = [
+  param('id').isNumeric(),
+  body('description', 'The description must have a valid value').optional().notEmpty().trim(),
+  body('genre', 'The genre must have a valid value').optional().notEmpty().trim(),
+  body('language', 'The language must have a valid value').optional().notEmpty().trim(),
+  body('name', 'The name must have a valid value').optional().notEmpty().trim(),
+  body('rating', 'The rating must be a float').optional().isFloat(),
+  body('status', 'The status must be an integer').optional().isInt(),
+  ...movieOptionalValidations
+]
+
+const moviePostValidations = [
+  body('description', 'The description is mandatory').notEmpty().trim(),
+  body('genre', 'The genre is mandatory').notEmpty().trim(),
+  body('language', 'The language is mandatory').notEmpty().trim(),
+  body('name', 'The name is mandatory').notEmpty().trim(),
+  body('rating', 'The rating must be a float').isFloat(),
+  ...movieOptionalValidations
+]
+
 export {
   createMovieInstanceService,
   createMovieService,
-  getLikeCountService
+  getLikeCountService,
+  moviePatchValidations,
+  moviePostValidations
 }
