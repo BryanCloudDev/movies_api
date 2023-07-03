1e3ca30c1cca76e90392745b894106f393b2563c
Bryan Portillo <bportillo701@gmail.com>
2023-07-02 19:07:49 -0600

    adding logic for validate like on create

--- middlewares/validateLikedMovie.ts
+++ middlewares/validateLikedMovie.ts
@@ -2,9 +2,25 @@ import { type Response, type NextFunction } from 'express'
 import type ICustomRequest from '../dto/request/ICustomRequest'
 import { type User } from '../models'
 import { likedMoviesRepository } from '../repositories'
+import errorMessageHandler from '../services/errorMessage'
 
 const validateLikedMovieonCreate = async (req: ICustomRequest, res: Response, next: NextFunction): Promise<Response | undefined> => {
+  try {
+    const user = req.user as User
+    const movieId = parseInt(req.body.id)
 
+    const likedMovie = await likedMoviesRepository.findOne({ where: { movie: { id: movieId }, user: { id: user.id } } })
+
+    if (likedMovie !== null) {
+      return res.status(404).json({
+        message: `The movie with the id ${movieId} has received a like already`
+      })
+    }
+
+    next()
+  } catch (error: any) {
+    errorMessageHandler(error, 'Error in like a movie validation')
+  }
 }
 
 const validateLikedMovieonDelete = async (req: ICustomRequest, res: Response, next: NextFunction): Promise<Response | undefined> => {
@@ -15,7 +31,7 @@ const validateLikedMovieonDelete = async (req: ICustomRequest, res: Response, ne
 
   if (likedMovie === null) {
     return res.status(404).json({
-      message: `The movie with he id ${movieId} has not received a like from you`
+      message: `The movie with the id ${movieId} has not received a like from you`
     })
   }
 
