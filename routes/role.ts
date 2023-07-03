78dcdc79a347acb90659d5c89e9042473616ce9d
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 03:26:47 -0600

    adding middleware to delete endpoint

--- routes/role.ts
+++ routes/role.ts
@@ -2,7 +2,8 @@ import { Roles } from '../dto'
 import { Router } from 'express'
 import { body, param } from 'express-validator'
 import { createRole, deleteRole, getAllRoles } from '../controllers'
-import { validateFields, validateJWT, validateRole } from '../middlewares'
+import { validateFields, validateJWT, validateRole, validateRoleId } from '../middlewares'
+import validateRoleOnDelete from '../middlewares/role/validateRole'
 
 const roleRouter = Router()
 
@@ -11,7 +12,9 @@ roleRouter.delete('/:id',
     validateJWT,
     validateRole([Roles.ADMIN]),
     param('id').isNumeric(),
-    validateFields
+    validateFields,
+    validateRoleId,
+    validateRoleOnDelete
   ],
   deleteRole
 )
