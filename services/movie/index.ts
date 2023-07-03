36e31fc01ff961d74e783ffe4ae1e527245a7cf1
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 07:26:51 -0600

    adding more validations to movie

--- services/movie/index.ts
+++ services/movie/index.ts
@@ -37,30 +37,30 @@ const getLikeCountService = (movies: Movie[]): IMovieLikeCount[] => {
 }
 
 const movieOptionalValidations = [
-  body('countryOrigin', 'The country must have a valid value').optional().notEmpty().trim(),
-  body('director', 'The director must have a valid value').optional().notEmpty().trim(),
-  body('duration', 'The duration must have a valid value').optional().isInt(),
+  body('countryOrigin', 'The country must have a valid value').optional().notEmpty().isString().trim(),
+  body('director', 'The director must have a valid value').optional().notEmpty().isString().trim(),
+  body('duration', 'The duration must have a valid value').optional().isInt({ min: 1, max: 360 }),
   body('poster', 'The poster must be a valid URL').optional().isURL().trim(),
   body('releaseDate', 'The release date must be a valid date').optional().isISO8601()
 ]
 
 const moviePatchValidations = [
   param('id').isNumeric(),
-  body('description', 'The description must have a valid value').optional().notEmpty().trim(),
-  body('genre', 'The genre must have a valid value').optional().notEmpty().trim(),
-  body('language', 'The language must have a valid value').optional().notEmpty().trim(),
-  body('name', 'The name must have a valid value').optional().notEmpty().trim(),
-  body('rating', 'The rating must be a float').optional().isFloat(),
+  body('description', 'The description must have a valid value').optional().notEmpty().isLength({ max: 3000 }).trim(),
+  body('genre', 'The genre must have a valid value').optional().notEmpty().isString().trim(),
+  body('language', 'The language must have a valid value').optional().notEmpty().isString().isLength({ max: 30 }).trim(),
+  body('name', 'The name must have a valid value').optional().notEmpty().isString().isLength({ max: 30 }).trim(),
+  body('rating', 'The rating must be a float').optional().isFloat({ min: 0, max: 10 }),
   body('status', 'The status must be an integer').optional().isInt(),
   ...movieOptionalValidations
 ]
 
 const moviePostValidations = [
-  body('description', 'The description is mandatory').notEmpty().trim(),
-  body('genre', 'The genre is mandatory').notEmpty().trim(),
-  body('language', 'The language is mandatory').notEmpty().trim(),
-  body('name', 'The name is mandatory').notEmpty().trim(),
-  body('rating', 'The rating must be a float').isFloat(),
+  body('description', 'The description is mandatory').notEmpty().isLength({ max: 3000 }).trim(),
+  body('genre', 'The genre is mandatory').notEmpty().isString().trim(),
+  body('language', 'The language is mandatory').notEmpty().isString().isLength({ max: 30 }).trim(),
+  body('name', 'The name is mandatory').notEmpty().isString().isLength({ max: 30 }).trim(),
+  body('rating', 'The rating must be a float between 0 and 10').isFloat({ min: 0, max: 10 }),
   ...movieOptionalValidations
 ]
 
