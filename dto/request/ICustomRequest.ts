e634ca45964e05bb8e248f5ee548f2878063c2cb
Bryan Portillo <bportillo701@gmail.com>
2023-07-02 18:55:34 -0600

    adding like movie prop for custom request

--- dto/request/ICustomRequest.ts
+++ dto/request/ICustomRequest.ts
@@ -1,7 +1,8 @@
 import { type Request } from 'express'
-import { type Role, type Movie } from '../../models'
+import { type Role, type Movie, type LikedMovie } from '../../models'
 
 export default interface ICustomRequest extends Request {
   movie: Movie
   role: Role
+  like: LikedMovie
 }
