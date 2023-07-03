530e9b597420985769ae6ccb31e3084908084472
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 02:21:44 -0600

    adding validate User to index

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,12 +1,14 @@
+import { validateLikedMovieonCreate, validateLikedMovieonDelete } from './validateLikedMovie'
 import validateEmailInChange from './validateEmailInChange'
 import validateFields from './validateFields'
 import validateIdMovie from './validateIdMovie'
 import validateJWT from './validateJWT'
-import { validateLikedMovieonCreate, validateLikedMovieonDelete } from './validateLikedMovie'
 import validateQuery from './validateQuery'
 import validateRole from './validateRole'
 import validateRoleOnCreate from './validateRoleOnCreate'
 import validateStatus from './validateStatus'
+import validateUserId from './validateUserId'
+import validateUserOnDelete from './validateUser'
 
 export {
   validateEmailInChange,
@@ -18,5 +20,7 @@ export {
   validateQuery,
   validateRole,
   validateRoleOnCreate,
-  validateStatus
+  validateStatus,
+  validateUserId,
+  validateUserOnDelete
 }
