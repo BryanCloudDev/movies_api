e6f545b1d8f39d03ebae1e06ddbb9ce4223c7334
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 00:39:00 -0600

    creating create role instance and role

--- services/role/index.ts
+++ services/role/index.ts
@@ -1,16 +1,37 @@
+import { type IRoleRequest } from '../../dto'
 import { type Role } from '../../models'
 import { roleRepository } from '../../repositories'
 import errorMessageHandler from '../errorMessage'
 
-const getRoleByIdService = async (id: number): Promise<Role | null | undefined | { message: string }> => {
+const getRoleByIdService = async (id: number): Promise<Role | null> => {
   try {
     const role = await roleRepository.findOne({ where: { id } })
     return role
   } catch (error: any) {
-    return errorMessageHandler(error, 'Error in get role by id service')
+    throw new Error(errorMessageHandler(error, 'Error in get role by id service').message)
+  }
+}
+
+const createRoleInstanceService = (roleRequest: IRoleRequest): Role => {
+  try {
+    const role = roleRepository.create({ ...roleRequest })
+    return role
+  } catch (error: any) {
+    throw new Error(errorMessageHandler(error, 'Error in create role instance').message)
+  }
+}
+
+const createRoleService = async (movie: Role): Promise<Role> => {
+  try {
+    const role = await roleRepository.save(movie)
+    return role
+  } catch (error: any) {
+    throw new Error(errorMessageHandler(error, 'Error in save role').message)
   }
 }
 
 export {
+  createRoleInstanceService,
+  createRoleService,
   getRoleByIdService
 }
