0f9ab8b6af2f7164c74825dbb96b3122467b2a41
Bryan Portillo <bportillo701@gmail.com>
2023-07-03 00:38:42 -0600

    adding services index

---
+++ services/index.ts
@@ -0,0 +1,36 @@
+import { checkIfRoleIsSent, createUserInstanceService, createUserService, emailExists, existsUserById, getUserbyIdService, userValidationRules } from './user'
+import { checkPassword, encrypt, generateJWT } from './auth'
+import { createLikedMovieInstanceService, createLikedMovieService } from './likedMovie'
+import { createMovieInstanceService, createMovieService, getLikeCountService } from './movie'
+import { createRoleInstanceService, createRoleService, getRoleByIdService } from './role'
+import { createUriComponent } from './utils/utils'
+import createFilter from './createFilter'
+import errorMessageHandler from './errorMessage'
+import passport from './passport/localStrategy'
+import swaggerDocs from './swagger'
+
+export {
+  checkIfRoleIsSent,
+  createFilter,
+  createMovieService,
+  createRoleInstanceService,
+  createRoleService,
+  createUriComponent,
+  createUserInstanceService,
+  createUserService,
+  emailExists,
+  errorMessageHandler,
+  existsUserById,
+  getLikeCountService,
+  getRoleByIdService,
+  getUserbyIdService,
+  passport,
+  swaggerDocs,
+  userValidationRules,
+  checkPassword,
+  createLikedMovieInstanceService,
+  createLikedMovieService,
+  createMovieInstanceService,
+  encrypt,
+  generateJWT
+}
