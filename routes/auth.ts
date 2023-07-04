e287b7e70a6d948135a002760c829151258c29fe
Bryan Portillo <bportillo701@gmail.com>
2023-07-04 00:16:58 -0600

    adding docs for login controller

--- routes/auth.ts
+++ routes/auth.ts
@@ -5,6 +5,47 @@ import { validateFields } from '../middlewares'
 
 const authRouter = Router()
 
+/**
+ * @swagger
+ *
+ * /login:
+ *  post:
+ *    tags:
+ *    - Login
+ *    summary: Login for users.
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/LoginCredentials'
+ *    responses:
+ *      200:
+ *        content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/BearerToken'
+ *      400:
+ *        description: Bad request
+ *        content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/FailedLogin'
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              oneOf:
+ *              - $ref: '#/components/schemas/UserDeleted'
+ *              - $ref: '#/components/schemas/UserBanned'
+ *              - $ref: '#/components/schemas/UserUnauthorized'
+ *      422:
+ *        description: Unprocessable entity
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UnprocessableEntity'
+ */
 authRouter.post('/',
   [
     body('email', 'The email address is not valid').isEmail().trim(),
@@ -13,5 +54,31 @@ authRouter.post('/',
   ],
   login
 )
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LoginCredentials:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           format: email
+ *         password:
+ *           type: string
+ *     BearerToken:
+ *       type: object
+ *       properties:
+ *         token:
+ *           type: string
+ *           example: Bearer Token used for authentication
+ *     FailedLogin:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: Email or passowrd incorrect
+ */
 
 export default authRouter
