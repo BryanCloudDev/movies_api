43735368d208cf6e9b96b7d636a1d8338aab73f3
Bryan Portillo <bportillo701@gmail.com>
2023-07-04 00:24:14 -0600

    adding docs for delete role controller

--- routes/role.ts
+++ routes/role.ts
@@ -6,6 +6,55 @@ import { validateFields, validateJWT, validateQuery, validateRole, validateRoleI
 
 const roleRouter = Router()
 
+/**
+ * @swagger
+ *
+ * /roles/{id}:
+ *  delete:
+ *    tags:
+ *    - Role
+ *    summary: Deletes a role by id.
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: Movie id to be deleted.
+ *        schema:
+ *          type: integer
+ *          format: int64
+ *          minimum: 1
+ *    responses:
+ *      204:
+ *        description: No content
+ *      400:
+ *        description: Bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/RoleAlreadyInactive'
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              oneOf:
+ *              - $ref: '#/components/schemas/UserDeleted'
+ *              - $ref: '#/components/schemas/UserBanned'
+ *              - $ref: '#/components/schemas/UserUnauthorized'
+ *              - $ref: '#/components/schemas/UnprocessableEntity'
+ *      404:
+ *        description: Role not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/RoleNotFound'
+ *      422:
+ *        description: Unprocessable entity
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UnprocessableEntity'
+ */
 roleRouter.delete('/:id',
   [
     validateJWT,
@@ -35,5 +84,29 @@ roleRouter.post('/',
   ],
   createRole
 )
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     RoleAlreadyInactive:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: The role has been marked already as inactive
+ *     RoleNotFound:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: The role the id 5 does not exist
+ *     FailedLogin:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: Email or passowrd incorrect
+ */
 
 export default roleRouter
